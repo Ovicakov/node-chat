6aafc3ea2dd4b7706d0bfe699a9f1032089c0581
Ovicakov <clement.ovic@gmail.com>
2026-04-27 19:41:43 +0200

    remove unused type

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,5 +1,5 @@
 import { type FastifyInstance } from "fastify";
-import { usersSchema, type UsersBody } from "./users.schema.ts";
+import { usersSchema } from "./users.schema.ts";
 import usersHandler from "./users.handler.ts";
 
 export default async function (app: FastifyInstance) {
