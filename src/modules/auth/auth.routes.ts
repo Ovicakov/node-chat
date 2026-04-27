c99c72b8f22e9e92bc4f1f2f9e6da5f82a185d95
Ovicakov <clement.ovic@gmail.com>
2026-04-27 19:38:50 +0200

    remove generique types from routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -8,28 +8,16 @@ import {
   loginSchema,
   refreshTokenSchema,
   signupSchema,
-  type LoginBody,
-  type RefreshTokenBody,
-  type SignupBody,
 } from "./auth.schema.ts";
-import authenticate from "../../hooks/authenticate.ts";
 
 export default async function (app: FastifyInstance) {
-  app.post<{ Body: LoginBody }>(
-    "/login",
-    { schema: { body: loginSchema } },
-    loginHandler(app),
-  );
+  app.post("/login", { schema: { body: loginSchema } }, loginHandler(app));
 
-  app.post<{ Body: RefreshTokenBody }>(
+  app.post(
     "/refresh",
     { schema: { body: refreshTokenSchema } },
     refreshTokenHandler(app),
   );
 
-  app.post<{ Body: SignupBody }>(
-    "/signup",
-    { schema: { body: signupSchema } },
-    signupHandler(app),
-  );
+  app.post("/signup", { schema: { body: signupSchema } }, signupHandler(app));
 }
--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -3,9 +3,5 @@ import { usersSchema, type UsersBody } from "./users.schema.ts";
 import usersHandler from "./users.handler.ts";
 
 export default async function (app: FastifyInstance) {
-  app.post<{ Body: UsersBody }>(
-    "/users",
-    { schema: { body: usersSchema } },
-    usersHandler(app),
-  );
+  app.post("/users", { schema: { body: usersSchema } }, usersHandler(app));
 }
